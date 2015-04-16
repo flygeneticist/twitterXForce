ad9f547704f8ad4fb32a6b74970fda7848d45c64
Kevin Keller <flygeneticist@gmail.com>
2015-04-16 14:23:58 +0700

    Logic added for post route, twitter api wrapper script utilized, and settings array created to hold env vars.

--- web/index.php
+++ web/index.php
@@ -3,10 +3,18 @@
 require('../vendor/autoload.php');
 require_once('TwitterAPIExchange.php');
 // Access tokens are stored as environment varibales on Heroku server.
-$access_token = getenv('access_token');
-$access_token_secret = getenv('access_token_secret');
-$consumer_key = getenv('consumer_key');
-$consumer_key_secret = getenv('consumer_key_secret');
+$settings = array(
+    'oauth_access_token' => getenv('access_token'),
+    'access_token_secret' => getenv('access_token_secret'),
+    'consumer_key' => getenv('consumer_key'),
+    'consumer_key_secret' => getenv('consumer_key_secret')
+)
+
+// setup for twitter API request
+$url = 'https://api.twitter.com/1.1/followers/list.json';
+$getfield = '?skip_status=true&count=200&username=';
+$requestMethod = 'GET';
+$twitter = new TwitterAPIExchange($settings);
 
 $app = new Application();
 $app['debug'] = true;
@@ -29,23 +37,29 @@ $app->get('/twitter', function() use($app) {
 
 $app->post('/twitter', function() use($app) {
     $app['monolog']->addDebug('logging POST Twitter page output.');
-    users = array($data);
-    for (usr in users) {
-        get_followers(usr)
+    if ($_POST['users']) {
+        users = array();
+        for (usr in users) {
+            get_followers(usr)
+        }
+        return 'Processing your Twitter POST request.';
+    } else {
+        return 'Users were not supplied correctly.';
     }
-    return 'On Twitter POST page.';
 });
-
-$app->run();
 
 /* HELPER FUNCTIONS */
 function get_followers(user) {
     followers = array();
-    pages = http_get('https://api.twitter.com/1.1/followers/list.json?
-        screen_name='.$usr.'&count=200&skip_status=true');
     do {
-      // grab and append users to array
+        // grab and append users to array
+        res = $twitter ->setGetfield($getfield)
+                       ->buildOauth($url, $requestMethod)
+                       ->performRequest();
     } while (pages.next_cursor != null);
 }
+
+/* Run the application */
+$app->run();
 
 ?>
