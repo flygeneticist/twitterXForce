335b24016c4fbc77e040f8564935d70edd543c2a
Kevin Keller <flygeneticist@gmail.com>
2015-04-21 17:52:00 +0700

    Simplified code, added mem/wait conditions, and tweaked cursor usage.

--- web/index.php
+++ web/index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('max_execution_time', 600);
+ini_set('memory_limit', '1024M');
 
 require ('../vendor/autoload.php');
 require_once ('TwitterAPIExchange.php');
@@ -24,14 +26,13 @@ $app->get('/twitter', function () use ($app) {
 
 $app->post('/twitter', function () use ($app) {
 		echo '<h3>Here are the results of your API call</h3><br /><p>'.$data.'</p>';
-		$data = array();
 		$app['monolog']->addDebug('logging POST Twitter page output.');
 		$users = $_POST['users'];
 		if ($users != null && $users != '') {
 			$users = explode(',', $users);
 			foreach ($users as $usr) {
 				echo '<div><p>Followers of: '.$usr.'</p>';
-				array_push($data, get_followers($usr));
+				get_followers($usr);
 				echo '</div>';
 			}
 			return '<h3>That\'s all the data!</h3>';
@@ -42,8 +43,6 @@ $app->post('/twitter', function () use ($app) {
 
 /* HELPER FUNCTIONS */
 function get_followers($usr) {
-	// Empty array to store all Jsonified follower data
-	$followers = array();
 	// Access tokens are stored as environment varibales on Heroku server.
 	$settings = array(
 		'consumer_key'              => getenv('consumer_key'),
@@ -56,20 +55,22 @@ function get_followers($usr) {
 	$twitter       = new TwitterAPIExchange($settings);
 	$url           = urlencode('https://api.twitter.com/1.1/followers/list.json');
 	$requestMethod = urlencode('GET');
-	$getField      = '?count=200&screen_name=';
-	$cursor        = -1;
-	$getField_u    = urlencode($getField.$usr.'&skip_status=1');
+	$count         = 'count=200';
+	$cursor        = '-1';
+	$sn_pre        = 'screen_name=';
+	$skip          = 'skip_status=1';
 	do {
 		// grab the data from the API and store as associative array
-		$res = json_decode($twitter->setGetfield($getField_u)
-			->buildOauth($url, $requestMethod)->performRequest()
-			, $assoc = TRUE);
-		array_push($followers, $res);
-		$cursor = $res['next_cursor'];
+		$res = json_decode($twitter->setGetfield(urlencode('?'.$count.'&'.$cursor.'&'.$sn_pre.$usr.'&'.$skip))
+			->buildOauth($url, $requestMethod)->performRequest(),
+			$assoc = TRUE);
 		// write out data
-		echo '<pre>';
+		echo '<pre>Results for: ';
+		print_r($cursor);
+		echo '<br />';
 		print_r($res);
 		echo '</pre>';
+		$cursor = $res['next_cursor'];
 	} while ($cursor != 0);
 	return $followers;
 }
